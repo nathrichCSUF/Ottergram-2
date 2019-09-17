0f04ff2be4965852812b857c910b901cb5ff7706
nathrichCSUF <31288234+nathrichCSUF@users.noreply.github.com>
2019-09-17 00:06:37 -0700

    Fixed Email in Author Comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-//Author: Nathaniel Richards nathrich@3@csu.fullerton.edu CWID 891101883
+//Author: Nathaniel Richards nathrich23@csu.fullerton.edu CWID 891101883
 //CPSC 349 Assignment #2: Visual Effects
 //Due 9/16/2019
 var DETAIL_IMAGE_SELECTOR = "[data-image-role=\"target\"]";
